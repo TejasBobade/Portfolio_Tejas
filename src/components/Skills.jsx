62d8c8ba389b1871019c90339107b893e294c6e1
Tejas <tejasbobade2002@gmail.com>
2025-01-11 12:37:27 +0530

    Added a component skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import html from "../assets/html.png"
+import css from "../assets/css.png"
+import javascript from "../assets/javascript.png"
+import tailwind from "../assets/tailwind.png"
+import react from "../assets/react.png"
+
+
+const Skills = () => {
+  return (
+    <div className='border border-gray-600 bg-black text-gray-400 md:h-[150px] max-w-[1200px] mx-auto grid grid-cols-6 place-items-center md:flex md:justify-between md:items-center'>
+
+        <h2 className='text-gray-700 text-xl md:text-4xl font-bold m-4'>
+            My <br /> Tech <br /> Stack
+        </h2>
+
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px'>
+            <img src={html} alt="html" />
+            <p className='mt-2'>html</p>
+        </div>
+
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px'>
+            <img src={css} alt="css" />
+            <p className='mt-2'>css</p>
+        </div>
+
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px'>
+            <img src={tailwind} alt="tailwind" />
+            <p className='mt-2'>tailwind</p>
+        </div>
+
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px'>
+            <img src={javascript} alt="javascript" />
+            <p className='mt-2'>javascript</p>
+        </div>
+
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px'>
+            <img src={react} alt="react" />
+            <p className='mt-2'>react</p>
+        </div>
+    </div>
+  )
+}
+
+export default Skills;
